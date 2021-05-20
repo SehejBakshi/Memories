e7ed276c85fc4739f9a664d3c968e112087cf724
SehejBakshi <sehej.bakshi@yahoo.in>
2021-05-20 09:34:58 +0530

    commit

---
+++ server/index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import postRoutes from './routes/posts.js';
+import dotenv from 'dotenv';
+
+const app = express();
+dotenv.config();
+
+app.use(express.json({limit: "30mb", extended: true}))
+app.use(express.urlencoded({limit: "30mb", extended: true}))
+app.use(cors());
+
+app.use('/posts', postRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Hello to memories API');
+});
+
+const PORT = process.env.PORT;
+
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology:true})
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
